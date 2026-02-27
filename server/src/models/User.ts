9bc34d5d7209e31cccb4cbe1006bfb7bd2344d51
swapneethpunna <swapneethpunna@msitprogram.net>
2026-02-27 23:29:06 +0530

    refactor: migrate backend from JavaScript to TypeScript

---
+++ server/src/models/User.ts
@@ -0,0 +1,28 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const userSchema: Schema<IUser> = new Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model<IUser>("User", userSchema);
